d60959b6557265f0a215e0502f8f118b1a30f91d
Alan <alangyho@gmail.com>
2015-04-15 11:07:35 -1000

    Finished beginning static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
+  root 'static_pages#home'
   get 'static_pages/dashboard'
-
   get 'static_pages/categories'
 
   # The priority is based upon order of creation: first created -> highest priority.
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Open Class Knowledge Base on Rails"
   end
 
   test "should get dashboard" do
     get :dashboard
     assert_response :success
+    assert_select "title", "Dashboard | Open Class Knowledge Base on Rails"
   end
 
   test "should get categories" do
     get :categories
     assert_response :success
+    assert_select "title", "Categories | Open Class Knowledge Base on Rails"
   end
 
 end
