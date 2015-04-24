ce8baf03db3e7ef59f0705c92be6109f0c933dcb
Alan <alangyho@gmail.com>
2015-04-24 13:01:55 -1000

    FriendlyId for human-friendly url strings

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,8 +3,8 @@ class CategoryController < ApplicationController
   end
 
   def show
-  	@category = Category.find(params[:id])
-  	@postList = Post.where(category_id: Category.find(params[:id]))
+  	@category = Category.friendly.find(params[:id])
+  	@postList = Post.where(category_id: Category.friendly.find(params[:id]))
   	@sticky = @postList.where(sticky: true)
   	@challenge = @postList.where(challenge: true)
   	@posts = @postList.where.not(id: @sticky).where.not(id: @challenge).paginate(:page => params[:page], :per_page => 10)
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+	extend FriendlyId
+	friendly_id :title, use: :slugged
 end
